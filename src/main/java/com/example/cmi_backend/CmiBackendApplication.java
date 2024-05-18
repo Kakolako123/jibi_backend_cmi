e78962e4c63740ffe6a5c44942687753708e82f6
ibtissam3 <ibtibtissamamine@gmail.com>
2024-05-18 21:50:00 +0100

    Added by Ibtissam

---
+++ src/main/java/com/example/cmi_backend/CmiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmiBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cmi_backend/entity/PaymentAccount.java
@@ -0,0 +1,23 @@
+package com.example.cmi_backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class PaymentAccount {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long paymentAccountId;
+    @OneToOne
+    @JoinColumn(name="customer_id", referencedColumnName="id")
+    private Customer customer;
+    private double accountBalance;
+    private LocalDate createdDate;
+    private String bankName;
+    private String PhoneNumber;
+}
